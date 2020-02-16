31fd6695222dea697827cb8e1e145b9cbeb3cea8
ozanuslan <ozanuslan@hotmail.com>
2020-02-16 17:31:59 +0300

    New functions.

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,19 @@ public class Main {
     static String PATH;
 
     public static void main(String[] args) {
-        PATH = "/home/ozan/Downloads/input.txt";
+        PATH = "C:\\Users\\Ucak\\Downloads\\input.txt";
         InputFile.open(PATH);
-        for (int i = 0; i < InputFile.METADATA.length; i++) {
-            System.out.println(InputFile.METADATA[i]);
+        InputFile.splitData();
+        InputFile.fillCountryHashtable();
+        InputFile.fillSportHashtable();
+        /*
+        for (int i = 0; i < InputFile.ATHLETES.length; i++) {
+            for (int j = 0; j < InputFile.ATHLETES[i].length; j++) {
+                System.out.print(InputFile.ATHLETES[i][j] + " ");
+            }
+            System.out.println();
         }
+        */
+        
     }
 }
