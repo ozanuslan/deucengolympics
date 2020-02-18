152f8aa7318efdcbd86bf516a77491c1f8881ea5
ozanuslan <ozanuslan@hotmail.com>
2020-02-18 11:15:37 +0300

    Country data print

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,14 @@ public class Main {
         InputFile.open(PATH);
         InputFile file = new InputFile();
         file.fillAthleteData();
-
+        file.fillCountryData();
         for (int i = 0; i < file.ATHLETES.length; i++) {
             System.out.println(file.ATHLETES[i].getCountry() + " " + file.ATHLETES[i].getSport() + " "
                     + file.ATHLETES[i].getName() + " " + file.ATHLETES[i].getGender() + " "
                     + file.ATHLETES[i].getBirthDate() + " " + file.ATHLETES[i].getSkill());
+        }
+        for(int i = 0; i<file.COUNTRIES.length; i++){
+            System.out.println(file.COUNTRIES[i].country+" "+file.COUNTRIES[i].point);
         }
     }
 }
