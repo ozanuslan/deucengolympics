2e0fcac545b48f3d96a03d7c7151c1700a117b7e
ozanuslan <ozanuslan@hotmail.com>
2020-02-21 11:50:04 +0300

    changed from static to regular

--- src/Date.java
+++ src/Date.java
@@ -1,11 +1,12 @@
 public class Date{
-    public static int DAY = 24; //Initial day
-    public static int MONTH = 7; //Initial month //Ask how can I make these variables TOTALLY GLOBAL
-    public static int YEAR = 2020; //Initial year
+    int day; //Initial day
+    int month; //Initial month 
+    int year; //Initial year
     int hour;
     int min;
 
     public Date(){
 
     }
+
 }
