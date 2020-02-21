158be8d84b0dc6c67d0128551ec445812e2a8e4a
ozanuslan <ozanuslan@hotmail.com>
2020-02-21 11:52:00 +0300

    kick started the generateFixture

--- src/Olympiad.java
+++ src/Olympiad.java
@@ -1,6 +1,21 @@
 public class Olympiad {
-    public static void generateFixture() {
-
+    static Tournament[] TOURNAMENTS;
+    static Sport[] SPORTS;
+    static int tournamentCount;
+    public Olympiad(Sport[] SPORTS_ARR){
+        SPORTS = SPORTS_ARR;
+        tournamentCount = SPORTS.length;
+        TOURNAMENTS = new Tournament[tournamentCount];
+    }
+    public static Tournament[] generateFixture() {
+        for(int i = 0; i < tournamentCount; i++){
+            int startingDay = 24;
+            int startingMonth = 7;
+            int startingYear = 2020;
+            TOURNAMENTS[i] = new Tournament(SPORTS[i]);
+        }
+        
+        return TOURNAMENTS;
     }
 
     public static void performTournament() {
