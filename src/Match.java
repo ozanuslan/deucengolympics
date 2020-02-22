deb9da23910ec8a5280c72d1a8ebe8e095bbbb32
ozanuslan <ozanuslan@hotmail.com>
2020-02-22 19:07:39 +0300

    Point incrementation related tasks

--- src/Match.java
+++ src/Match.java
@@ -1,46 +1,54 @@
 import java.util.Random;
 
 public class Match {
-    String sport;
     Athlete ath1;
     Athlete ath2;
     String winner;
     Date date;
     Random rnd = new Random();
 
-    public Match(String sport, Athlete ath1, Athlete ath2) {
-        this.sport = sport;
+    public Match(Athlete ath1, Athlete ath2) {
         this.ath1 = ath1;
         this.ath2 = ath2;
-    }
-
-    public String getSport() {
-        return sport;
     }
 
     public String getWinner() {
         return winner;
     }
 
-    private void setWinner(String winner) {
+    private String setWinner(String winner) {
         this.winner = winner;
+        return this.winner;
     }
 
-    public void determineWinner() {
+    private void incrementCountryPoint(Athlete ath) {
+        for (int i = 0; i < Olympiad.COUNTRIES.length; i++) {
+            if (ath.getCountry().equals(Olympiad.COUNTRIES[i].country)) {
+                Olympiad.COUNTRIES[i].incrementPoint();
+                break;
+            }
+        }
+    }
+
+    public String determineWinner() {
         if (ath1.skill > ath2.skill) {
             ath1.incrementPoint();
-            setWinner(ath1.name);
+            incrementCountryPoint(ath1);
+            return setWinner(ath1.name);
         } else if (ath2.skill > ath1.skill) {
             ath2.incrementPoint();
-            setWinner(ath2.name);
+            incrementCountryPoint(ath2);
+            return setWinner(ath2.name);
         } else {
             double dice = rnd.nextDouble();
             if (dice >= 0.50) {
                 ath1.incrementPoint();
-                setWinner(ath1.name);
+                incrementCountryPoint(ath1);
+                return setWinner(ath1.name);
             } else {
                 ath2.incrementPoint();
-                setWinner(ath2.name);
+                incrementCountryPoint(ath2);
+                return setWinner(ath2.name);
             }
         }
     }
