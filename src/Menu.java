4d5e61c67c31201a982c33d29fe9952ea3201374
ozanuslan <ozanuslan@hotmail.com>
2020-02-24 11:28:18 +0300

    Primary menu functions are now working

--- src/Menu.java
+++ src/Menu.java
@@ -4,19 +4,49 @@ import java.util.Scanner;
 public class Menu {
     static Scanner sc = new Scanner(System.in);
 
-    private static void printFixture() {
-
+    private void printFixture() {
+        for(int i = 0; i<Main.olympiad.TOURNAMENTS.length; i++){
+            System.out.println(Main.olympiad.TOURNAMENTS[i].sport.sport+" Fixture");
+            for(int j = 0; j<Main.olympiad.TOURNAMENTS[i].MATCHES.length; j++){
+                System.out.print(Main.olympiad.TOURNAMENTS[i].MATCHES[j].date.day+".");
+                System.out.print(Main.olympiad.TOURNAMENTS[i].MATCHES[j].date.month+".");
+                System.out.print(Main.olympiad.TOURNAMENTS[i].MATCHES[j].date.year+" ");
+                System.out.print(Main.olympiad.TOURNAMENTS[i].MATCHES[j].date.hour+":");
+                System.out.print(Main.olympiad.TOURNAMENTS[i].MATCHES[j].date.min+" ");
+                System.out.print(Main.olympiad.TOURNAMENTS[i].MATCHES[j].ath1.country+"-");
+                System.out.print(Main.olympiad.TOURNAMENTS[i].MATCHES[j].ath1.name+" vs. ");
+                System.out.print(Main.olympiad.TOURNAMENTS[i].MATCHES[j].ath2.country+"-");
+                System.out.print(Main.olympiad.TOURNAMENTS[i].MATCHES[j].ath2.name+"  ");
+                System.out.println();
+            }
+            System.out.println();
+        }
     }
 
-    private static void printTournaments() {
-
+    private void printTournaments() {
+        for(int i = 0; i<Main.olympiad.TOURNAMENTS.length; i++){
+            System.out.println(Main.olympiad.TOURNAMENTS[i].sport.sport+" Matches");
+            for(int j = 0; j<Main.olympiad.TOURNAMENTS[i].MATCHES.length; j++){
+                System.out.print(Main.olympiad.TOURNAMENTS[i].MATCHES[j].date.day+".");
+                System.out.print(Main.olympiad.TOURNAMENTS[i].MATCHES[j].date.month+".");
+                System.out.print(Main.olympiad.TOURNAMENTS[i].MATCHES[j].date.year+" ");
+                System.out.print(Main.olympiad.TOURNAMENTS[i].MATCHES[j].date.hour+":");
+                System.out.print(Main.olympiad.TOURNAMENTS[i].MATCHES[j].date.min+" ");
+                System.out.print(Main.olympiad.TOURNAMENTS[i].MATCHES[j].ath1.country+"-");
+                System.out.print(Main.olympiad.TOURNAMENTS[i].MATCHES[j].ath1.name+" vs. ");
+                System.out.print(Main.olympiad.TOURNAMENTS[i].MATCHES[j].ath2.country+"-");
+                System.out.print(Main.olympiad.TOURNAMENTS[i].MATCHES[j].ath2.name+"  ");
+                System.out.println("Winner: "+Main.olympiad.TOURNAMENTS[i].MATCHES[j].winner);
+            }
+            System.out.println();
+        }
     }
 
-    private static void showStatistics() {
+    private void showStatistics() {
         showSecondaryMenu();
     }
 
-    public static void showPrimaryMenu() {
+    public void showPrimaryMenu() {
         System.out.println("*****MENU*****");
         System.out.println("1- Generate Fixture");
         System.out.println("2- Perform Tournaments");
@@ -25,20 +55,23 @@ public class Menu {
         boolean isLegal = false;
         while (!isLegal) {
             ans = sc.nextLine();
-            if (ans == "1" || ans == "2" || ans == "3") {
+            if (ans.equals("1") || ans.equals("2") || ans.equals("3")) {
                 isLegal = true;
             }
         }
         if (ans.equals("1")) {
             printFixture();
+            showPrimaryMenu();
         } else if (ans.equals("2")) {
             printTournaments();
+            showPrimaryMenu();
         } else if (ans.equals("3")) {
             showStatistics();
         }
+        
     }
 
-    public static void showSecondaryMenu() {
+    public void showSecondaryMenu() {
         System.out.println("***** STATISTICS MENU *****");
         System.out.println("1- The country with the highest score");
         System.out.println("2- The athlete with the highest score");
@@ -49,12 +82,16 @@ public class Menu {
         boolean isLegal = false;
         while (!isLegal) {
             ans = sc.nextLine();
-            if (ans == "1" || ans == "2" || ans == "3" || ans == "4") {
+            if (ans.equals("1") || ans.equals("2") || ans.equals("3") || ans.equals("4")) {
                 isLegal = true;
             }
         }
         /*
          * todo: add menu options
          */
+    }
+
+    public void printCountryWithHighestScore(){
+    
     }
 }
