42a7980860774b41b8748652e6a01beeb78c4cda
ozanuslan <ozanuslan@hotmail.com>
2020-02-24 11:27:58 +0300

    Now menu class is being used to display the info

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
     static String PATH;
-
+    static Olympiad olympiad;
     public static void main(String[] args) {
         PATH = "input.txt";
         InputFile.open(PATH);
@@ -11,34 +11,9 @@ public class Main {
         file.fillAthleteData();
         Sport[] sports_arr = file.fillSportAthlete();
         Country[] country_arr = file.fillCountryData();
-        Olympiad olympiad = new Olympiad(sports_arr, country_arr);
+        olympiad = new Olympiad(sports_arr, country_arr);
         olympiad.generateTourments();
-
-        for(int i = 0; i<olympiad.TOURNAMENTS.length; i++){
-            System.out.println(olympiad.TOURNAMENTS[i].sport.sport+" Fixture");
-            for(int j = 0; j<olympiad.TOURNAMENTS[i].MATCHES.length; j++){
-                System.out.print(olympiad.TOURNAMENTS[i].MATCHES[j].date.day+".");
-                System.out.print(olympiad.TOURNAMENTS[i].MATCHES[j].date.month+".");
-                System.out.print(olympiad.TOURNAMENTS[i].MATCHES[j].date.year+" ");
-                System.out.print(olympiad.TOURNAMENTS[i].MATCHES[j].date.hour+":");
-                System.out.print(olympiad.TOURNAMENTS[i].MATCHES[j].date.min+" ");
-                System.out.print(olympiad.TOURNAMENTS[i].MATCHES[j].ath1.country+"-");
-                System.out.print(olympiad.TOURNAMENTS[i].MATCHES[j].ath1.name+" vs. ");
-                System.out.print(olympiad.TOURNAMENTS[i].MATCHES[j].ath2.country+"-");
-                System.out.print(olympiad.TOURNAMENTS[i].MATCHES[j].ath2.name+"  ");
-                System.out.println("Winner: "+olympiad.TOURNAMENTS[i].MATCHES[j].winner);
-            }
-            System.out.println();
-        }
-        /*
-        for (int i = 0; i < file.ATHLETES.length; i++) {
-            System.out.println(file.ATHLETES[i].getCountry() + " " + file.ATHLETES[i].getSport() + " "
-                    + file.ATHLETES[i].getName() + " " + file.ATHLETES[i].getGender() + " "
-                    + file.ATHLETES[i].getBirthDate() + " " + file.ATHLETES[i].getSkill());
-        }
-        for (int i = 0; i < file.COUNTRIES.length; i++) {
-            System.out.println(Olympiad.COUNTRIES[i].getCountry() + " " + Olympiad.COUNTRIES[i].getPoint());
-        }
-        */
+        Menu menu = new Menu();
+        menu.showPrimaryMenu();
     }
 }
